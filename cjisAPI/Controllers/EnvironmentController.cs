0fd3597d44002fa0823a8cb321abaad33e7f81f2
Bill Jameson <Bill.Jameson@claytoncountyga.gov>
2020-03-30 00:02:46 -0400

    read connection string from environment variable

---
+++ cjisAPI/Controllers/EnvironmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+
+namespace cjisAPI.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class EnvironmentController : ControllerBase
+  {
+    private readonly ILogger<MobileController> _logger;
+
+    public EnvironmentController(ILogger<MobileController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public string GetConnectionString() {
+      return Environment.GetEnvironmentVariable("CJIS_API_CONNECTION_STRING");
+    }
+  }
+}
